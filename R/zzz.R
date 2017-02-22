3e5a4e4463197f30650e75446b05041cdda4c42b
paleo13 <jeffrey.hanson@uqconnect.edu.au>
2017-02-22 12:55:41 +1000

    fix bug where package doesnt load if gurobi isnt installed

--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,7 @@
 .pkgenv <- new.env(parent=emptyenv())
 
 .onAttach <- function(libname, pkgname) {
-  if (!requireNamespace('gurobi'))
+  if (!requireNamespace('gurobi', quietly = TRUE))
     packageStartupMessage('warning: the "gurobi" R package is not installed.',
                           'Once installed, gurobi can be used solve problems very quickly.',
                           'Academics can obtain a license at no cost.',
